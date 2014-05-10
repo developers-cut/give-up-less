6b578e0f8e933b1151c7d17c121e68e3d81a1d5a
Thomas Silva <thomas@royalsflush.com>
2014-05-10 18:51:43 +0100

    Initial commit for Give Up Less app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+GiveUpLess::Application.config.secret_token = '4e7d384831ee46f2e8a1527f39a9e1256eac756aee2e80d9b0605d0479d9091c2e875daf6297350c425e77006b53fc42f0add4f0a4d45e77ae4d66a456e9db41'
